71c939370f59da56b9d1de45d93a6bdf85f54012
ljjun <wisbyme@gmail.com>
2018-12-02 12:43:19 +0800

    update

--- vps/setup.sh
+++ vps/setup.sh
@@ -6,7 +6,7 @@ LANG=en_US.UTF-8
 is64bit=`getconf LONG_BIT`
 
 # 用法
-# rm -f /usr/bin/vps && wget -O /usr/bin/vps https://raw.githubusercontent.com/programs/scripts/master/vps/setup.sh && chmod +x /usr/bin/vps && vps
+# rm -f /usr/bin/vps && wget -N --no-check-certificate -q -O /usr/bin/vps https://raw.githubusercontent.com/programs/scripts/master/vps/setup.sh && chmod +x /usr/bin/vps && vps
 #
 GreenFont="\033[32m" && RedFont="\033[31m" && GreenBack="\033[42;37m" && RedBack="\033[41;37m" && FontEnd="\033[0m"
 Info="${GreenFont}[信息]${FontEnd}"
@@ -139,9 +139,9 @@ function installddos()
 		rf -f /usr/local/ddos/ignore.ip.list
 		rf -f /usr/local/ddos/ddos.sh
 
-		wget -q -O /usr/local/ddos/ddos.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/ddos.conf
-		wget -q -O /usr/local/ddos/ignore.ip.list https://raw.githubusercontent.com/programs/scripts/master/vps/config/ignore.ip.list
-		wget -q -O /usr/local/ddos/ddos.sh https://raw.githubusercontent.com/programs/scripts/master/vps/config/ddos.sh
+		wget -N --no-check-certificate -q -O /usr/local/ddos/ddos.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/ddos.conf
+		wget -N --no-check-certificate -q -O /usr/local/ddos/ignore.ip.list https://raw.githubusercontent.com/programs/scripts/master/vps/config/ignore.ip.list
+		wget -N --no-check-certificate -q -O /usr/local/ddos/ddos.sh https://raw.githubusercontent.com/programs/scripts/master/vps/config/ddos.sh
 		chmod 0755 /usr/local/ddos/ddos.sh
 		cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
 
@@ -202,7 +202,7 @@ function setupSsrmu()
 		/home/bin/ssrmu.sh
 
 		rm -f /usr/local/shadowsocksr/user-config.json
-		wget -q -O /usr/local/shadowsocksr/user-config.json https://raw.githubusercontent.com/programs/scripts/master/vps/config/user-config.json
+		wget -N --no-check-certificate -q -O /usr/local/shadowsocksr/user-config.json https://raw.githubusercontent.com/programs/scripts/master/vps/config/user-config.json
 
 		stty erase '^H' && read -p "SSR 是否使用 IPv6 配置? [Y/n] :" yn
 		[[ -z "${yn}" ]] && yn="y"
@@ -239,9 +239,9 @@ function installFrp()
 	rm -f /home/frp/frpstart
 	rm -f /home/frp/frps.ini
 
-	wget -q -O /home/frp/frps https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frps
-	wget -q -O /home/frp/frpstart https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frpstart
-	wget -q -O /home/frp/frps.ini https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frps.ini
+	wget -N --no-check-certificate -q -O /home/frp/frps https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frps
+	wget -N --no-check-certificate -q -O /home/frp/frpstart https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frpstart
+	wget -N --no-check-certificate -q -O /home/frp/frps.ini https://raw.githubusercontent.com/programs/scripts/master/vps/frp/frps.ini
 
 	if [ -f /home/frp/frps.ini ]; then
 
@@ -269,10 +269,10 @@ function setupServices()
 	rm -f /etc/supervisor/conf.d/frp.conf
 	rm -f /etc/iptables.up.rules
 
-	wget -q -O /etc/ssh/sshd_config https://raw.githubusercontent.com/programs/scripts/master/vps/config/sshd_config
-	wget -q -O /etc/fail2ban/jail.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/jail.conf
-	wget -q -O /etc/supervisor/conf.d/frp.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/frp.conf
-	wget -q -O /etc/iptables.up.rules https://raw.githubusercontent.com/programs/scripts/master/vps/config/iptables.up.rules
+	wget -N --no-check-certificate -q -O /etc/ssh/sshd_config https://raw.githubusercontent.com/programs/scripts/master/vps/config/sshd_config
+	wget -N --no-check-certificate -q -O /etc/fail2ban/jail.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/jail.conf
+	wget -N --no-check-certificate -q -O /etc/supervisor/conf.d/frp.conf https://raw.githubusercontent.com/programs/scripts/master/vps/config/frp.conf
+	wget -N --no-check-certificate -q -O /etc/iptables.up.rules https://raw.githubusercontent.com/programs/scripts/master/vps/config/iptables.up.rules
 
 	service sshd restart
 	service fail2ban restart
@@ -290,7 +290,7 @@ function setupBBR()
 {
 	if [ ! -f /home/bin/bbr.sh ]; then
 		rm -f /home/bin/bbr.sh
-		wget -q -O /home/bin/bbr.sh https://raw.githubusercontent.com/programs/scripts/master/vps/bbr.sh
+		wget -N --no-check-certificate -q -O /home/bin/bbr.sh https://raw.githubusercontent.com/programs/scripts/master/vps/bbr.sh
 		chmod +x /home/bin/bbr.sh
 	fi
 	/home/bin/bbr.sh
@@ -317,7 +317,7 @@ function do_speedtest()
 	/home/bin/zbanch.sh
 
 	rm -f /home/bin/superbench.sh
-	wget -q -O /home/bin/superbench.sh ${sbanch_url}
+	wget -N --no-check-certificate -q -O /home/bin/superbench.sh ${sbanch_url}
 	chmod +x /home/bin/superbench.sh
 
 	stty erase '^H' && read -p "是否需要进一步进行网络测试? [Y/n]:" yn
@@ -332,7 +332,7 @@ function do_bbrstatus()
 {
 	if [ ! -f /home/bin/bbr.sh ]; then
 		rm -f /home/bin/bbr.sh
-		wget -q -O /home/bin/bbr.sh https://raw.githubusercontent.com/programs/scripts/master/vps/bbr.sh
+		wget -N --no-check-certificate -q -O /home/bin/bbr.sh https://raw.githubusercontent.com/programs/scripts/master/vps/bbr.sh
 		chmod +x /home/bin/bbr.sh
 	fi
 	/home/bin/bbr.sh status
@@ -493,7 +493,7 @@ function do_sshkeys()
 			echo -e "${Tip}正在配置 SSH 授权密钥环境..."
 
 			rm -f ~/.ssh/authorized_keys
-			wget -q -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/programs/scripts/master/vps/config/authorized_keys
+			wget -N --no-check-certificate -q -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/programs/scripts/master/vps/config/authorized_keys
 			
 			${fsudo} chmod 400 ~/.ssh/authorized_keys
 			${fsudo} chattr +i ~/.ssh/authorized_keys
@@ -580,10 +580,15 @@ function do_removenq()
 
 function do_uninsdocker()
 {
+	if [ ! -f /usr/bin/docker ]; then
+		echo -e "${Info}已移除 Docker!" && exit 1
+	fi
+
 	stty erase '^H' && read -p "正在移除之前已安装的 Docker 版本，请确定? [Y/n]:" yn
 	[[ -z "${yn}" ]] && yn="y"
 	if [[ $yn == [Yy] ]]; then
 
+		${fsudo} systemctl stop docker
 		${fsudo} systemctl disable docker
 		stty erase '^H' && read -p "是否保留原有的 Docker 镜像或容器? [Y/n]:" ynt
 		[[ -z "${ynt}" ]] && ynt="y"
@@ -603,75 +608,81 @@ function do_makedocker()
 		echo -e "${Error}请使用64位系统安装 Docker!" && exit 1
 	fi
 
-	stty erase '^H' && read -p "继续之前 将先移除之前可能已安装的 Docker 版本，请确定? [Y/n]:" yn
-	[[ -z "${yn}" ]] && yn="y"
-	if [[ $yn == [Yy] ]]; then
+	if [ -f /usr/bin/docker ]; then
+		stty erase '^H' && read -p "继续之前 将先移除之前可能已安装的 Docker 版本，请确定? [Y/n]:" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ $yn == [Yy] ]]; then
 
-		${fsudo} systemctl disable docker
-		stty erase '^H' && read -p "是否保留原有的 Docker 镜像或容器? [Y/n]:" ynt
-		[[ -z "${ynt}" ]] && ynt="y"
-		if [[ $ynt == [Yy] ]]; then
-			${fsudo} sudo apt-get purge docker-ce
+			${fsudo} systemctl stop docker
+			${fsudo} systemctl disable docker
+			stty erase '^H' && read -p "是否保留原有的 Docker 镜像或容器? [Y/n]:" ynt
+			[[ -z "${ynt}" ]] && ynt="y"
+			if [[ $ynt == [Yy] ]]; then
+				${fsudo} sudo apt-get purge docker-ce
+			else
+				${fsudo} apt-get remove -y docker docker-engine docker.io
+				${fsudo} rm -rf /var/lib/docker
+			fi
 		else
-			${fsudo} apt-get remove -y docker docker-engine docker.io
-			${fsudo} rm -rf /var/lib/docker
+			echo -e "${Error}暂时无法安装 Docker!" && exit 1
 		fi
-
-		${fsudo} apt-get update
-		${fsudo} apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
-
-		#SET UP THE REPOSITORY
-		if [ ${release} != "debian" ]; then
-			${fsudo} apt-get install -y --no-install-recommends gnupg2
-			curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-		fi
-		if [ ${release} != "ubuntu" ]; then
-			curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		fi
-		${fsudo} apt-key fingerprint 0EBFCD88
-
-		if [ ${release} != "debian" ]; then
-			${fsudo} add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-		fi
-		if [ ${release} != "ubuntu" ]; then
-			${fsudo} add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-		fi
-		
-		#INSTALL DOCKER CE
-		${fsudo} apt-get install -y docker.io #docker-ce docker-engine
-		
-		#设置权限
-		dockeruser=${defaultuser}
-		${fsudo} groupadd docker
-		${fsudo} usermod -aG docker ${dockeruser}
-
-		# https://github.com/docker/compose/releases
-		${fsudo} curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-		${fsudo} chmod +x /usr/local/bin/docker-compose
-
-		stty erase '^H' && read -p "是否设置国内镜像加速? [y/N]:" ynn
-		[[ -z "${ynn}" ]] && ynn="n"
-		if [[ $ynn == [Yy] ]]; then
-			#设置镜像加速
-			${fsudo} mkdir -p /etc/docker
-			${fsudo} tee /etc/docker/daemon.json <<-'EOF'
-			{
-			"registry-mirrors": ["https://registry.docker-cn.com"]
-			}
-			EOF
-		fi
-		${fsudo} systemctl enable docker
-		${fsudo} systemctl daemon-reload
-		${fsudo} systemctl restart docker
-
-		curl -fsSL https://get.docker.com -o /home/bin/get-docker.sh
-		#${fsudo} bash /home/bin/get-docker.sh
-
-		${fsudo} docker run hello-world
-		echo -e "${Info}已完成 DOCKER 运行环境的配置!"
-	else
-		echo -e "${Error}暂时无法安装 Docker!" && exit 1
 	fi
+	
+	${fsudo} apt-get update
+	${fsudo} apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+
+	#SET UP THE REPOSITORY
+	if [ ${release} != "debian" ]; then
+		${fsudo} apt-get install -y --no-install-recommends gnupg2
+		curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	fi
+	if [ ${release} != "ubuntu" ]; then
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	fi
+	${fsudo} apt-key fingerprint 0EBFCD88
+
+	if [ ${release} != "debian" ]; then
+		${fsudo} add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	fi
+	if [ ${release} != "ubuntu" ]; then
+		${fsudo} add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	fi
+		
+	#INSTALL DOCKER CE
+	${fsudo} apt-get install -y docker.io #docker-ce docker-engine
+		
+	#设置权限
+	dockeruser=${defaultuser}
+	${fsudo} groupadd docker
+	${fsudo} usermod -aG docker ${dockeruser}
+
+	# https://github.com/docker/compose/releases
+	${fsudo} rm -f /usr/local/bin/docker-compose
+	${fsudo} curl -fsSL https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	${fsudo} chmod +x /usr/local/bin/docker-compose
+
+	stty erase '^H' && read -p "是否设置国内镜像加速? [y/N]:" ynn
+	[[ -z "${ynn}" ]] && ynn="n"
+	if [[ $ynn == [Yy] ]]; then
+		#设置镜像加速
+		${fsudo} mkdir -p /etc/docker
+		${fsudo} tee /etc/docker/daemon.json <<-'EOF'
+		{
+		"registry-mirrors": ["https://registry.docker-cn.com"]
+		}
+		EOF
+	fi
+	${fsudo} systemctl enable docker
+	${fsudo} systemctl daemon-reload
+	${fsudo} systemctl restart docker
+
+	curl -fsSL https://get.docker.com -o /home/bin/get-docker.sh
+	#${fsudo} bash /home/bin/get-docker.sh
+
+	${fsudo} docker run hello-world
+	${fsudo} docker rm `${fsudo} docker ps -a |awk '{print $1}' | grep [0-9a-z]` 
+	${fsudo} docker rmi hello-world
+	echo -e "${Info}已完成 DOCKER 运行环境的配置!"
 }
 
 #主程序入口
