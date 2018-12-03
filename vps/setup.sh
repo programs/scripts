6825b37b494ce8f7f90fce19c46e7656a928a987
ljjun <wisbyme@gmail.com>
2018-12-03 22:11:52 +0800

    update

--- vps/setup.sh
+++ vps/setup.sh
@@ -199,7 +199,7 @@ function createSwap()
 	if [ "x${need_swap}" == "xdo" ]; then
 		
 		echo -e "${Info}当前物理内存为${GreenBack} ${tram_size}M ${FontEnd}"
-		tty erase '^H' && read -p "请输入将要创建交换分区大小 (默认等于物理内存的一半) :" inputsize
+		stty erase '^H' && read -p "请输入将要创建交换分区大小 (默认等于物理内存的一半) :" inputsize
 		[[ -z "${inputsize}" ]] && inputsize=`expr ${tram_size} / 2`
 		dd if=/dev/zero of=${swapfile} bs=${inputsize}M count=1
 
