708a6e95396b58c5ca3e98aa3afcff312f497c80
ljjun <wisbyme@gmail.com>
2018-12-09 16:14:06 +0800

    update

--- vps/setup.sh
+++ vps/setup.sh
@@ -680,8 +680,8 @@ function do_ensshkeys()
 		if [ -d ~/.ssh ]; then
 			echo -e "${Tip}正在配置 SSH 授权密钥环境..."
 
-			[[ ~/.ssh/authorized_keys ]] && rm -f ~/.ssh/authorized_keys
-			wget -N --no-check-certificate -q -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/programs/scripts/master/vps/config/authorized_keys
+			[[ ~/.ssh/authorized_keys ]] && ${fsudo} rm -f ~/.ssh/authorized_keys
+			${fsudo} wget -N --no-check-certificate -q -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/programs/scripts/master/vps/config/authorized_keys
 			
 			${fsudo} chmod 400 ~/.ssh/authorized_keys
 			${fsudo} chattr +i ~/.ssh/authorized_keys
